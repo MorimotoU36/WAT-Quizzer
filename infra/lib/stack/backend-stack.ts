828db956897fb3c860186d2121661a96baa787df
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-11-27 10:55:14 +0900

    インフラのコメント削除

--- infra/lib/stack/backend-stack.ts
+++ infra/lib/stack/backend-stack.ts
@@ -1,16 +1,7 @@
 import * as cdk from 'aws-cdk-lib'
-import { Secret } from 'aws-cdk-lib/aws-secretsmanager'
 import { Construct } from 'constructs'
 import * as dotenv from 'dotenv'
-import * as acm from 'aws-cdk-lib/aws-certificatemanager'
 import * as route53 from 'aws-cdk-lib/aws-route53'
-import { makeRecordsToALB } from '../service/route53'
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
-import * as ecs from 'aws-cdk-lib/aws-ecs'
-import * as ecr from 'aws-cdk-lib/aws-ecr'
-import * as log from 'aws-cdk-lib/aws-logs'
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'
-import { RemovalPolicy } from 'aws-cdk-lib'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
 
 dotenv.config()
@@ -27,149 +18,8 @@ export class BackendStack extends cdk.Stack {
       crossRegionReferences: true
     })
 
-    const { region, accountId } = new cdk.ScopedAws(this)
-
-    // Secret Manager の値
-    const secretJson = Secret.fromSecretAttributes(this, 'SecretJson', {
-      secretCompleteArn: `arn:aws:secretsmanager:${region}:${accountId}:secret:${
-        process.env.DB_URL_ID || ''
-      }-${process.env.DB_URL_SUFFIX || ''}`
-    })
-
-    // // Api Container
-    // //// VPC
-    // const vpc = new ec2.Vpc(this, 'Vpc', {
-    //   natGateways: 1,
-    //   subnetConfiguration: [
-    //     {
-    //       cidrMask: 24,
-    //       name: 'private',
-    //       subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
-    //     },
-    //     {
-    //       cidrMask: 24,
-    //       name: 'public',
-    //       subnetType: ec2.SubnetType.PUBLIC
-    //     }
-    //   ]
-    // })
-    // //// VPC Endpoint
-    // vpc.addInterfaceEndpoint('ecr-endpoint', {
-    //   service: ec2.InterfaceVpcEndpointAwsService.ECR
-    // })
-    // vpc.addInterfaceEndpoint('logs-endpoint', {
-    //   service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS
-    // })
-    // vpc.addInterfaceEndpoint('sm-endpoint', {
-    //   service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER
-    // })
-    // //// SecurityGroup
-    // const securityGroupELB = new ec2.SecurityGroup(this, 'SecurityGroupELB', {
-    //   vpc
-    // })
-    // securityGroupELB.addIngressRule(
-    //   ec2.Peer.ipv4('0.0.0.0/0'),
-    //   ec2.Port.tcp(80)
-    // )
-    // const securityGroupAPP = new ec2.SecurityGroup(this, 'SecurityGroupAPP', {
-    //   vpc
-    // })
-    // // ALB
-    // const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
-    //   vpc,
-    //   securityGroup: securityGroupELB,
-    //   internetFacing: true
-    // })
-    // // TargetGroup
-    // const targetGroup = new elbv2.ApplicationTargetGroup(this, 'TG', {
-    //   vpc: vpc,
-    //   port: 3000,
-    //   protocol: elbv2.ApplicationProtocol.HTTP,
-    //   targetType: elbv2.TargetType.IP,
-    //   healthCheck: {
-    //     path: '/',
-    //     healthyHttpCodes: '200'
-    //   }
-    // })
-    // // ALB Listener
-    // const listenerHTTPS = alb.addListener('ListenerHTTPS', {
-    //   port: 443,
-    //   certificates: [
-    //     elbv2.ListenerCertificate.fromCertificateManager(
-    //       apiCertificateAPNorthEast1
-    //     )
-    //   ]
-    // })
-    // listenerHTTPS.addTargetGroups('DefaultHTTPSResponse', {
-    //   targetGroups: [targetGroup]
-    // })
-
-    // //// ECS Cluster
-    // const cluster = new ecs.Cluster(this, 'Cluster', {
-    //   vpc
-    // })
-    //// Create ECR Repository
-    const ecrRepository = new ecr.Repository(this, 'EcrRepo', {
-      repositoryName: `${props.env}-backend-ecr`,
-      removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteImages: true
-    })
-    // //// Fargate
-    // const fargateTaskDefinition = new ecs.FargateTaskDefinition(
-    //   this,
-    //   'TaskDef',
-    //   {
-    //     memoryLimitMiB: 1024,
-    //     cpu: 512
-    //   }
-    // )
-    // const container = fargateTaskDefinition.addContainer(
-    //   'QuizzerBackendContainer',
-    //   {
-    //     image: ecs.ContainerImage.fromEcrRepository(ecrRepository),
-    //     logging: ecs.LogDrivers.awsLogs({
-    //       streamPrefix: `${props.env}-backend-api-ecs`,
-    //       logRetention: log.RetentionDays.ONE_MONTH
-    //     }),
-    //     secrets: {
-    //       DATABASE_URL: ecs.Secret.fromSecretsManager(
-    //         secretJson,
-    //         'DATABASE_URL'
-    //       )
-    //     },
-    //     environment: {
-    //       APP_ENV: 'local',
-    //       AWS_REGION: process.env.AWS_REGION || '',
-    //       AWS_COGNITO_USERPOOL_ID: process.env.AWS_COGNITO_USERPOOL_ID || '',
-    //       AWS_COGNITO_APPCLIENT_ID: process.env.AWS_COGNITO_APPCLIENT_ID || '',
-    //       // TODO これはやめる secretの方でやりたい
-    //       AWS_COGNITO_USERNAME: process.env.AWS_COGNITO_USERNAME || ''
-    //     }
-    //   }
-    // )
-    // container.addPortMappings({
-    //   containerPort: 4000,
-    //   hostPort: 4000
-    // })
-    // const service = new ecs.FargateService(this, 'Service', {
-    //   cluster,
-    //   taskDefinition: fargateTaskDefinition,
-    //   desiredCount: 1,
-    //   assignPublicIp: false,
-    //   securityGroups: [securityGroupAPP]
-    // })
-    // service.attachToApplicationTargetGroup(targetGroup)
-
-    // // make route53 record to ALB domain
-    // makeRecordsToALB(
-    //   this,
-    //   process.env.API_DOMAIN_NAME || '',
-    //   alb,
-    //   props.hostedZone
-    // )
-
     // SAM  API
-    const samApi = apigw.RestApi.fromRestApiId(
+    apigw.RestApi.fromRestApiId(
       this,
       'SamBackendApi',
       process.env.SAM_APIGATEWAY_ID || ''
