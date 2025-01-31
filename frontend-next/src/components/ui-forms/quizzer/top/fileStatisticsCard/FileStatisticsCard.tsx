7ba6cab238706ab398b023beba610b444d43f367
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-02-01 00:44:15 +0900

    問題ファイル正解数統計をchartjsに置き換え　やっとまともになった

--- frontend-next/src/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard.tsx
+++ frontend-next/src/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard.tsx
@@ -1,10 +1,12 @@
-import Chart from 'react-google-charts';
-import styles from '../../../../Chart.module.css';
 import { Card } from '@/components/ui-elements/card/Card';
 import { getQuizFileStatisticsDataAPI, QuizFileStatisticsApiResponse } from 'quizzer-lib';
 import { useEffect, useState } from 'react';
+import { CircularProgress } from '@mui/material';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
 interface FileStatisticsCardProps {}
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const FileStatisticsCard = ({}: FileStatisticsCardProps) => {
   const [quizFileStatisticsData, setQuizFileStatisticsData] = useState<QuizFileStatisticsApiResponse[]>([]);
@@ -16,25 +18,45 @@ export const FileStatisticsCard = ({}: FileStatisticsCardProps) => {
     })();
   }, []);
 
-  const data: [string, string | number, string | number, string | number][] = quizFileStatisticsData.map((x) => {
-    return [x.file_nickname, x.count, x.clear, x.fail];
-  });
-  data.unshift(['問題ファイル名', '問題数', '正解数', '不正解数']);
+  const data = {
+    labels: quizFileStatisticsData.map((x) => {
+      return x.file_nickname;
+    }),
+    datasets: [
+      {
+        label: '問題数',
+        data: quizFileStatisticsData.map((x) => x.count),
+        backgroundColor: 'royalblue'
+      },
+      {
+        label: '正解数',
+        data: quizFileStatisticsData.map((x) => x.clear),
+        backgroundColor: 'crimson'
+      },
+      {
+        label: '不正解数',
+        data: quizFileStatisticsData.map((x) => x.fail),
+        backgroundColor: 'black'
+      }
+    ]
+  };
+
   const options = {
-    chart: {
-      title: '問題ファイル統計',
-      subtitle: '(基礎)問題数,正解不正解数'
-    },
-    vAxis: {
-      viewWindow: {
-        min: 0
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const
+      },
+      title: {
+        display: true,
+        text: '問題ファイル統計'
       }
     }
   };
 
   return (
     <Card variant="outlined" attr="margin-vertical">
-      <Chart chartType="Bar" data={data} options={options} className={styles.quiz_file} />
+      {quizFileStatisticsData.length > 0 ? <Bar options={options} data={data} /> : <CircularProgress />}
     </Card>
   );
 };
