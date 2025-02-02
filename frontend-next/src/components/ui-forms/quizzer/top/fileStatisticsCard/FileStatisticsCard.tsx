070c8d40d606591604a9790a36defd6e26e5c726
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-02-02 14:26:18 +0900

    quizzer 正答率グラフ表示をファイルごと表示に変更・未解答問題数も表示可能に

--- frontend-next/src/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard.tsx
+++ frontend-next/src/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard.tsx
@@ -1,67 +1,84 @@
 import { Card } from '@/components/ui-elements/card/Card';
-import { getQuizFileStatisticsDataAPI, QuizFileStatisticsApiResponse } from 'quizzer-lib';
+import { getQuizFileStatisticsDataAPI, PullDownOptionDto, QuizFileStatisticsApiResponse } from 'quizzer-lib';
 import { useEffect, useState } from 'react';
 import { CircularProgress } from '@mui/material';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { PullDown } from '@/components/ui-elements/pullDown/PullDown';
 
 interface FileStatisticsCardProps {}
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const FileStatisticsCard = ({}: FileStatisticsCardProps) => {
+  const [filelistoption, setFilelistoption] = useState<PullDownOptionDto[]>([]);
+  const [selectedFile, setSelectedFile] = useState<number>(1);
   const [quizFileStatisticsData, setQuizFileStatisticsData] = useState<QuizFileStatisticsApiResponse[]>([]);
+  const selectedFileStaticsData = quizFileStatisticsData.find((x) => x.file_num === selectedFile);
 
   useEffect(() => {
     (async () => {
       const result = await getQuizFileStatisticsDataAPI({});
       result.result && setQuizFileStatisticsData(result.result as QuizFileStatisticsApiResponse[]);
+      // 問題ファイルプルダウン ファイル統計リスト取得から作成
+      result.result &&
+        setFilelistoption(
+          (result.result as QuizFileStatisticsApiResponse[]).map((x) => {
+            return {
+              value: String(x.file_num),
+              label: x.file_nickname
+            } as PullDownOptionDto;
+          })
+        );
     })();
   }, []);
 
+  const labels = ['正解数', '不正解数', '未解答数'];
+  const datasets = selectedFileStaticsData
+    ? [
+        {
+          label: '正答率分布',
+          data: [
+            selectedFileStaticsData.clear || 0,
+            selectedFileStaticsData.fail || 0,
+            selectedFileStaticsData.not_answered || 0
+          ],
+          backgroundColor: ['crimson', 'black', 'dimgray']
+        }
+      ]
+    : [];
   const data = {
-    labels: quizFileStatisticsData.map((x) => {
-      return x.file_nickname;
-    }),
-    datasets: [
-      {
-        label: '問題数',
-        data: quizFileStatisticsData.map((x) => x.count),
-        backgroundColor: 'royalblue'
-      },
-      {
-        label: '正解数',
-        data: quizFileStatisticsData.map((x) => x.clear),
-        backgroundColor: 'crimson'
-      },
-      {
-        label: '不正解数',
-        data: quizFileStatisticsData.map((x) => x.fail),
-        backgroundColor: 'black'
-      },
-      {
-        label: '未解答数',
-        data: quizFileStatisticsData.map((x) => x.not_answered),
-        backgroundColor: 'dimgray'
-      }
-    ]
+    labels,
+    datasets
   };
 
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const
+        position: 'left' as const
       },
       title: {
         display: true,
-        text: '問題ファイル統計'
+        text: `問題ファイル統計(${selectedFileStaticsData ? selectedFileStaticsData.file_nickname : 'null'}): ${
+          selectedFileStaticsData ? selectedFileStaticsData.count : '0'
+        }問中`
       }
     }
   };
 
   return (
-    <Card variant="outlined" attr="margin-vertical">
-      {quizFileStatisticsData.length > 0 ? <Bar options={options} data={data} /> : <CircularProgress />}
-    </Card>
+    <>
+      <Card variant="outlined" attr="margin-vertical">
+        {/*TODO quizzer形式のプルダウン結構使うから　あらかじめquizzerのファイル値が入った状態のプルダウンをコンポーネントとして用意したほうがいい気した */}
+        <PullDown
+          label={'問題ファイル'}
+          optionList={filelistoption}
+          onChange={(e) => setSelectedFile(+e.target.value)}
+        />
+        <Card variant="outlined" attr="rect-600 margin-vertical">
+          {quizFileStatisticsData.length > 0 ? <Doughnut data={data} options={options} /> : <CircularProgress />}
+        </Card>
+      </Card>
+    </>
   );
 };
