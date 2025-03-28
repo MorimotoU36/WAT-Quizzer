c22c88cbbf6b3b5eeba9a360928be4670b6c2f8d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-03-29 04:13:44 +0900

    ロジックミス修正

--- batch/src/tools/avg_line.register.ts
+++ batch/src/tools/avg_line.register.ts
@@ -10,10 +10,10 @@ if (process.argv.length !== 4) {
     `Error: Usage: npx ts-node ${__filename} (全ファイルの行数合計) (全ファイル数)`
   )
   process.exit(1)
-} else if (!isNaN(Number(process.argv[2]))) {
+} else if (isNaN(Number(process.argv[2]))) {
   console.error(`Error: ${process.argv[2]} は数字ではありません。`)
   process.exit(1)
-} else if (!isNaN(Number(process.argv[3]))) {
+} else if (isNaN(Number(process.argv[3]))) {
   console.error(`Error: ${process.argv[3]} は数字ではありません。`)
   process.exit(1)
 }
