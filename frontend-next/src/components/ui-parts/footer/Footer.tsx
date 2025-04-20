1ab2f05fde8f9a7d38609aaeeef267f948176329
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-04-20 14:08:42 +0900

    フッターにログアウト処理追加

--- frontend-next/src/components/ui-parts/footer/Footer.tsx
+++ frontend-next/src/components/ui-parts/footer/Footer.tsx
@@ -3,17 +3,33 @@ import React from 'react';
 import styles from './Footer.module.css';
 import { FooterBar } from '@/components/ui-elements/footerBar/FooterBar';
 import { Button } from '@/components/ui-elements/button/Button';
+import { useRouter } from 'next/router';
 
 interface FooterProps {
   bgColor: string;
   topHref: string;
 }
 
-export const Footer = ({ bgColor = '#0077B6', topHref }: FooterProps) => (
-  <FooterBar bgColor={bgColor}>
-    <span className={styles.left}>
-      <Button attr="no-margin no-border" size="small" color="inherit" href={topHref} label="トップ" />
-    </span>
-    <span className={styles.right}>©️ Tatsuroh Wakasugi</span>
-  </FooterBar>
-);
+export const Footer = ({ bgColor = '#0077B6', topHref }: FooterProps) => {
+  const router = useRouter();
+
+  return (
+    <FooterBar bgColor={bgColor}>
+      <span className={styles.left}>
+        <Button attr="no-margin no-border" size="small" color="inherit" href={topHref} label="トップ" />
+        <Button
+          attr="no-margin no-border"
+          size="small"
+          color="inherit"
+          onClick={(e) => {
+            // TODO 別関数にして別ファイルに置く？
+            localStorage.removeItem('idToken');
+            router.push('/login');
+          }}
+          label="ログアウト"
+        />
+      </span>
+      <span className={styles.right}>©️ Tatsuroh Wakasugi</span>
+    </FooterBar>
+  );
+};
