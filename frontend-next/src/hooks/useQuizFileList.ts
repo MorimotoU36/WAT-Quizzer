66297bc697c996629e29320964ae72600b8d0db0
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-06-17 21:37:44 +0900

    useEffectでプルダウン用の問題ファイルリスト取る箇所　別ファイル（フック）化

---
+++ frontend-next/src/hooks/useQuizFileList.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { messageState } from '@/atoms/Message';
+import {
+  GetQuizFileApiResponseDto,
+  getQuizFileListAPI,
+  PullDownOptionDto,
+  quizFileListAPIResponseToPullDownAdapter
+} from 'quizzer-lib';
+
+export const useQuizFileList = () => {
+  const [filelistoption, setFilelistoption] = useState<PullDownOptionDto[]>([]);
+  const setMessage = useSetRecoilState(messageState);
+
+  useEffect(() => {
+    (async () => {
+      const result = await getQuizFileListAPI();
+      setMessage(result.message);
+      const pullDownOption = result.result
+        ? quizFileListAPIResponseToPullDownAdapter(result.result as GetQuizFileApiResponseDto[])
+        : [];
+      setFilelistoption(pullDownOption);
+    })();
+  }, [setMessage]);
+
+  return { filelistoption };
+};
--- frontend-next/src/pages/quizzer/index.tsx
+++ frontend-next/src/pages/quizzer/index.tsx
@@ -4,38 +4,16 @@ import { PullDown } from '@/components/ui-elements/pullDown/PullDown';
 import { FileStatisticsCard } from '@/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard';
 import { QuizAnswerLogStatisticsCard } from '@/components/ui-forms/quizzer/top/quizAnswerLogStatisticsCard/QuizAnswerLogStatisticsCard';
 import { Container } from '@mui/material';
-import {
-  GetQuizFileApiResponseDto,
-  getQuizFileListAPI,
-  PullDownOptionDto,
-  quizFileListAPIResponseToPullDownAdapter
-} from 'quizzer-lib';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import styles from '../../components/Chart.module.css';
 import { AccuracyRateHistgramCard } from '@/components/ui-forms/quizzer/top/accuracyRateHistgramCard/AccuracyRateHistgramCard';
+import { useQuizFileList } from '@/hooks/useQuizFileList';
 
-type Props = {
-  isMock?: boolean;
-};
-
-export default function QuizzerTopPage({ isMock }: Props) {
-  const [filelistoption, setFilelistoption] = useState<PullDownOptionDto[]>([]);
+export default function QuizzerTopPage() {
+  const { filelistoption } = useQuizFileList();
   const [selectedFileNum, setSelectedFileNum] = useState<number>(-1);
   const setMessage = useSetRecoilState(messageState);
-
-  useEffect(() => {
-    (async () => {
-      // 問題ファイルプルダウン ファイル統計リスト取得から作成
-      // TODO 問題ファイルプルダウンは割とどこでも使うから　このuseEffectの動作含めて別のコンポーネントに切り分けた方が良い気した
-      const result = await getQuizFileListAPI();
-      setMessage(result.message);
-      const pullDownOption = result.result
-        ? quizFileListAPIResponseToPullDownAdapter(result.result as GetQuizFileApiResponseDto[])
-        : [];
-      setFilelistoption(pullDownOption);
-    })();
-  }, [setMessage]);
 
   const contents = () => {
     return (
