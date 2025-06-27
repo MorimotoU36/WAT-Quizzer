23589a5a2c27e32736c29224f130ce33dba30236
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-06-27 18:31:29 +0900

    Container storyファイル作成

---
+++ frontend-next/src/components/ui-elements/container/Container.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Container } from './Container';
+
+const meta: Meta<typeof Container> = {
+  title: 'ui-elements/container/Container',
+  component: Container,
+  argTypes: {
+    attr: {
+      control: 'array',
+      description: 'クラス名の配列'
+    },
+    children: {
+      control: 'text',
+      description: '子要素'
+    }
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof Container>;
+
+export const Default: Story = {
+  args: {
+    children: 'デフォルトのコンテナ'
+  }
+};
+
+export const WithAttr: Story = {
+  args: {
+    attr: ['root', 'customClass'],
+    children: '複数クラス名を指定した例'
+  }
+};
