aac289d9ef39c9d170a536654b10e42cd83bfd15
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-07-25 00:57:43 +0900

    トグルボタン　storybookファイル作成

---
+++ frontend-next/src/components/ui-elements/toggleButton/ToggleButton.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ToggleButton } from './ToggleButton';
+import React, { useState } from 'react';
+
+const meta = {
+  title: 'Atom/ToggleButton',
+  component: ToggleButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered'
+  }
+} satisfies Meta<typeof ToggleButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Main = {
+  render: () => <ToggleButtonStory />
+} as unknown as Story;
+
+const ToggleButtonStory = () => {
+  const [alignment, setAlignment] = React.useState('left');
+  return <ToggleButton alignment={alignment} setAlignment={setAlignment} buttonValues={['left', 'center', 'right']} />;
+};
