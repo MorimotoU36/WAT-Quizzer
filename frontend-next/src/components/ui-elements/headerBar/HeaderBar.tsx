c5ad60454d4176d183cc673e12251b5f04fc8924
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-09-26 00:13:58 +0900

    HeaderBar,SideBarのz-inndex調整（レスポンシブ）

--- frontend-next/src/components/ui-elements/headerBar/HeaderBar.tsx
+++ frontend-next/src/components/ui-elements/headerBar/HeaderBar.tsx
@@ -6,7 +6,10 @@ interface HeaderBarProps {
 }
 
 export const HeaderBar = ({ bgColor = '#0077B6', ...props }: HeaderBarProps) => (
-  <header className="fixed top-0 w-full h-[44px] md:h-[48px] shadow-md" style={{ backgroundColor: bgColor }}>
+  <header
+    className="fixed top-0 w-full h-[44px] md:h-[48px] shadow-md"
+    style={{ backgroundColor: bgColor, zIndex: 10001 }}
+  >
     {props.children}
   </header>
 );
