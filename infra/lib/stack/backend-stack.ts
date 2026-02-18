2ea9a2018e19f9a32f23be84a20256982dd6f7d7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2026-02-18 21:10:39 +0900

    Todo管理用DynamoDB作成

--- infra/lib/stack/backend-stack.ts
+++ infra/lib/stack/backend-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs'
 import * as dotenv from 'dotenv'
 import * as route53 from 'aws-cdk-lib/aws-route53'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 
 dotenv.config()
 
@@ -12,6 +13,8 @@ type BackendStackProps = {
 }
 
 export class BackendStack extends cdk.Stack {
+  readonly todoCheckStatusTable: dynamodb.Table
+
   constructor(scope: Construct, id: string, props: BackendStackProps) {
     super(scope, id, {
       env: { region: process.env.REGION || '' },
@@ -24,5 +27,24 @@ export class BackendStack extends cdk.Stack {
       'SamBackendApi',
       process.env.SAM_APIGATEWAY_ID || ''
     )
+
+    // DynamoDB Table for Todo check status
+    // Partition key: userId (Cognito user ID)
+    // Sort key: date (YYYY-MM-DD format)
+    // Attributes: completedTodoIds (array of completed Todo IDs)
+    this.todoCheckStatusTable = new dynamodb.Table(this, `${props.env}TodoCheckStatusTable`, {
+      tableName: `${props.env}-todo-check-status`,
+      partitionKey: {
+        name: 'userId',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'date',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      pointInTimeRecovery: false
+    })
   }
 }
